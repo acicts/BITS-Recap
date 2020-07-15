36ab13ae6c26c2f239f06c3831f31453de313444
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-07-16 05:24:20 +0530

    bits 18 page

--- _site/js/chartsjs.js
+++ _site/js/chartsjs.js
@@ -1,11 +1,22 @@
+// Only Change these things...
+
+const allYears = [2018,2019,2020];
+const allTasks = [82,58,200];
+const allCodeTasks = [55,10,56];
+const allDesignTasks = [23,75,86];
+const allExloreTasks = [60,20,100];
+
+// Don't change anything below...
+
 var ctx = document.getElementById('alltasks').getContext('2d');
+
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['2018', '2019', '2020'],
+        labels: allYears,
         datasets: [{
             label: '# of Tasks',
-            data: [55, 100, 890],
+            data: allTasks,
             backgroundColor: [
                 'rgba(54, 162, 235, 0.4)'
             ],
@@ -30,24 +41,24 @@ var ctx2 = document.getElementById('allcatogery').getContext('2d');
 var myChart2 = new Chart(ctx2, {
     type: 'bar',
     data: {
-        labels: ['2018', '2019', '2020'],
+        labels: allYears,
         datasets: [{
             label: 'Code Tasks',
-            data: [50, 100, 65],
+            data: allCodeTasks,
             backgroundColor:  'rgba(153, 102, 255, 0.4)',
             borderColor: 'rgba(153, 102, 255, 1)',
             borderWidth: 1
         },
         {
             label: 'Design Tasks',
-            data: [50, 05, 99],
+            data: allDesignTasks,
             backgroundColor: 'rgba(255, 159, 64, 0.4)',
             borderColor: 'rgba(255, 159, 64, 1)',
             borderWidth: 1
         },
         {
             label: 'Explore Tasks',
-            data: [50, 65, 65],
+            data: allExloreTasks,
             backgroundColor:  'rgba(75, 192, 192, 0.4)',
             borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 1
